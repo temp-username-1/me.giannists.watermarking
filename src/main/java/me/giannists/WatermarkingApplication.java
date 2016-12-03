9a8712a840e56cec0fb2aeb39852806aa6c663f9
Yiannis Tsimalis <ioannis.tsimalis@number26.de>
2016-12-03 18:15:08 +0100

    initCommit

---
+++ src/main/java/me/giannists/WatermarkingApplication.java
@@ -0,0 +1,12 @@
+package me.giannists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermarkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermarkingApplication.class, args);
+	}
+}
